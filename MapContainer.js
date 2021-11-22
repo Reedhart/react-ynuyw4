fb3aefc26aced5ed18969abd4711a12a88727dfe
Reedhart <maximilien.hepp@gmail.com>
2021-11-23 00:41:25 +0100

    Initial commit

---
+++ MapContainer.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { compose, withStateHandlers } from "recompose";
+import { InfoWindow, withGoogleMap, withScriptjs, GoogleMap, Marker } from 'react-google-maps';
+
+
+const Map = compose(
+    withStateHandlers(() => ({
+        isMarkerShown: false,
+        markerPosition: null
+      }), {
+        onMapClick: ({ isMarkerShown }) => (e) => ({
+            markerPosition: e.latLng,
+            isMarkerShown:true
+        })
+      }),
+    withScriptjs,
+    withGoogleMap
+)
+    (props =>
+        <GoogleMap
+            defaultZoom={8}
+            defaultCenter={{ lat: -34.397, lng: 150.644 }}
+            onClick={props.onMapClick}
+        >
+            {props.isMarkerShown && <Marker position={props.markerPosition} />}
+
+        </GoogleMap>
+    )
+
+export default class MapContainer extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div style={{ height: '100%' }}>
+                <Map
+                    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAvcDy5ZYc2ujCS6TTtI3RYX5QmuoV8Ffw"
+                    loadingElement={<div style={{ height: `100%` }} />}
+                    containerElement={<div style={{ height: `400px` }} />}
+                    mapElement={<div style={{ height: `100%` }} />}
+                />
+            </div>
+        )
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ynuyw4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ynuyw4)
